b51c22092d59d1189ec2d441757bd5446c4cc1ee
tpark14 <vkpark14@gmail.com>
2018-11-15 16:47:56 -0700

    this is the final version of run.sh

--- run.sh
+++ run.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
-cd var/www/html
+cd /
+cd var
+cd www
+cd html
 
 echo "Content-type: text/html"
 echo ""
 
-echo "<html>"
-echo "<head>"
-echo "<title>Tori's Page</title>"
-echo "</head>" 
-echo "<body>"
-echo "<h1>Intriguing Information</h1>"
-echo "<p>Here is a link <a href="A.html"></a></p>" 
-echo "</body>" 
-echo "</html>" 
->> index.html
+a="<html>
+<head>
+<title>Tori's Page</title>
+</head>
+<body>
+<h1>Intriguing Information</h1>
+<p>Here is a link: <a href="A/A.html">A</a></p>
+</body>
+</html>"
+echo "$a" > index.html
 
 sudo mkdir A
 cd A 
 
 echo "Content-type: text/html"
 echo ""
-
-echo "<html>"
-echo "<head>" 
-echo "<title>A Page</title>" 
-echo "<body>"
-echo "<h1>A Information</h1>" 
-echo "</body>"
-echo "</html>" 
->> A.html
+b="<html>
+<head>
+<title>A Page</title>
+<body>
+<h1>A Information</h1>
+<p1>Such exciting and important things</p1>
+</body>
+</html>" 
+echo "$b" > A.html
 
